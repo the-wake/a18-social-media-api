e61aed1c6131f6670abbac2ecdcb54e823674bdf
Ben Martin <bmartin2009@gmail.com>
2022-02-12 18:28:13 -0600

    Starting on models

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,27 @@
 const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    min: [1, 'Please enter at least one character'],
+    max: [280, 'Maximum length exceeded. ({VALUE} characters vs. maximum 280.'],
+    required: true
+  },
+  username: [{ type: mongoose.Schema.Types.ObjectId, ref: 'thought' }],
+  createdAt: 'created_at', // I think this should work. Also, use getter to format timestamp on query.
+  restrictions: [userSchema],
+},
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  });
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+postSchema.virtual('friendCount').get(() => {
+  return this.friends.length;
+});
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose');
 
+// Could store the regex as a variable, or put it in a helper, depending on how we want to use it.
+// Also may want to add a unique validator since the unique field value isn't a validation. A few options at:
+// https://www.npmjs.com/package/mongoose-unique-validator
+
+// Alternatively, we may be able to do that with an await on our user constructor, using a second User.init() function to try + await User.create, which will catch the error if the address is already in use.
+
 const userSchema = new mongoose.Schema({
   name: { type: String, trimmed: true, unique: true, required: true },
-  email: { type: String, unique: true, required: true },  // Add Mongoose's email matching validation.
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: [validEmail, 'Please enter a valid email address.'],
+    match: [/^([\w\.-]+)@([\w\.-]+)\.([a-zA-Z0-9\.]{2,6})$/, 'Please enter a valid email address.']
+  },
   thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'thought' }],
   friends: [userSchema],
-});
+},
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  });
 
 const User = mongoose.model('User', userSchema);
+
+postSchema.virtual('friendCount').get(() => {
+  return this.friends.length;
+});
 
 module.exports = User;
